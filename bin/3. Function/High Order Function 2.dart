8ed13e9da629eebe33e8895ec45320ecf06bdbec
kid <kiddev92@gmail.com>
2024-02-21 14:29:09 +0700

    main

--- bin/3. Function/High Order Function 2.dart
+++ bin/3. Function/High Order Function 2.dart
@@ -1,5 +1,6 @@
 void main() {
 
+  //------------ EXAMPLE 1 Function as Argument
   highOrderFunction(Function(int) func) {
     return func(55);
   }
@@ -10,5 +11,23 @@ void main() {
 
   print(highOrderFunction(multipleBy2));
 
+  //------------ EXAMPLE 2 Function as Argument
 
+  highOrderFn(Function(String) func) {
+    var value = func("john");
+    print(value);
+  }
+
+  String sayHallo(String name) => "Hallo $name";
+
+  highOrderFn(sayHallo);
+
+  //------------ EXAMPLE 3 Function as Argument
+
+  highOrderReturnFn(int value) {
+    return multipleBy2(value);
+  }
+
+  int value = highOrderReturnFn(25);
+  print(value);
 }
